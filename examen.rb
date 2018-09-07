f19244c825072011a03fe9ec7948e7496fad6f8f
AlanCardiel <43057260+AlanCardiel@users.noreply.github.com>
2018-09-07 10:12:35 -0700

    Add files via upload

---
+++ examen.rb
@@ -0,0 +1,39 @@
+#II.
+#1
+class Banco
+  initialize (nombre,saldo,tipoCuenta)
+  @nombre = nombre
+  @saldo = saldo
+  @tipoCuenta = tipoCuenta
+end
+
+def nombre
+  return @nombre
+end
+
+def saldo
+  return @saldo
+end
+
+def tipoCuenta
+  return @tipoCuenta
+end
+
+def nombre=(nombre)
+  return @nombre = nombre
+end
+
+def saldo=(saldo)
+  return @saldo = saldo
+end
+
+def tipoCuenta
+  return @tipoCuenta = tipoCuenta
+end
+end
+
+  
+
+
+
+
